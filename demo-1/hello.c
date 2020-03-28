b33efd24b0b8e690df26786e04c65e2b2cd4d76f
crgarcia12 <crgarcia@live.com.ar>
2020-03-28 13:12:08 +0100

    demo-1 prints the ring

--- demo-1/hello.c
+++ demo-1/hello.c
@@ -1,6 +1,12 @@
+#include <stdint.h>
 #include <stdio.h>
 
 int main(void) {
-    //printf("Hello, world!\n");
+    uint64_t rcs = 0;
+    int ring;
+
+    asm ("mov %%cs, %0" : "=r" (rcs));
+    ring = (int) (rcs & 3);
+    printf("Hello, world. This program is running on ring %d!\n", ring);
     return 0;
 }
